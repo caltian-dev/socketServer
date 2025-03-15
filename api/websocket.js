ae9054cc87e053e53a6789df07018a88c2367108
Daiki Fujimoto <daikifujimoto.yato@gmail.com>
2025-03-15 09:42:24 -0400

    test3

--- api/websocket.js
+++ api/websocket.js
@@ -9,23 +9,31 @@ export const config = {
 };
 
 export default function handler(req, res) {
-  if (!wss) {
-    const server = res.socket.server;
-    wss = new Server({ server });
-
-    wss.on("connection", (ws) => {
-      console.log("Client connected");
-      ws.send("Hello from Vercel WebSocket Server!");
-
-      ws.on("message", (message) => {
-        console.log("Received:", message);
-      });
-
-      ws.on("close", () => {
-        console.log("Client disconnected");
-      });
-    });
+  if (res.socket.server.wss) {
+    console.log("WebSocket server is already running.");
+    res.end();
+    return;
   }
+
+  console.log("Starting WebSocket server...");
+
+  const server = res.socket.server;
+  wss = new Server({ server });
+
+  wss.on("connection", (ws) => {
+    console.log("Client connected");
+    ws.send("Hello from Vercel WebSocket Server!");
+
+    ws.on("message", (message) => {
+      console.log("Received:", message);
+    });
+
+    ws.on("close", () => {
+      console.log("Client disconnected");
+    });
+  });
+
+  res.socket.server.wss = wss;
 
   res.end();
 }
